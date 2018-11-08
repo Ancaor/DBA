9c918fc802653f1785280dc777bacaa450f5f859
Antonio <Ancaor@correo.ugr.es>
2018-11-08 21:15:31 +0100

    otra vez

--- GugleCar/src/practica2/AgentBattery.java
+++ GugleCar/src/practica2/AgentBattery.java
@@ -1,4 +1,4 @@
-package guglecar;
+package practica2;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
--- GugleCar/src/practica2/AgentGPS.java
+++ GugleCar/src/practica2/AgentGPS.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package guglecar;
+package practica2;
 
 import com.eclipsesource.json.Json;
 import com.eclipsesource.json.JsonObject;
